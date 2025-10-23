f109245e835eaf030ebbfda164d78736ca7d2071
daniel-yapura <daniel.yapura.ungs@gmail.com>
2025-10-23 02:04:29 -0300

    feat: Implementar el tablero de juego
    Se agrega el método dibujarTablero() a la clase Juego.
    Este método renderiza una grilla dinámica de 9x5 que se ajusta
    automáticamente al ancho de la ventana y se alinea en la parte
    inferior de la pantalla.

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -17,7 +17,7 @@ public class Juego extends InterfaceJuego
 	Juego()
 	{
 		// Inicializa el objeto entorno
-		this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
+		this.entorno = new Entorno(this, "Proyecto para TP", 810, 600);
 		
 		// Inicializar lo que haga falta para el juego
 		// ...
@@ -37,8 +37,62 @@ public class Juego extends InterfaceJuego
 		// Procesamiento de un instante de tiempo
 		// ...
 		
+		dibujarTablero();
 	}
 	
+	void dibujarTablero() {
+	    // --- 1. DEFINIR PROPIEDADES ---
+	    int columnas = 9;
+	    int filas = 5;
+	    int anchoCasilla;
+	    int altoCasilla;
+	    int anchoTotalTablero;
+	    int altoTotalTablero;
+	       
+	    // --- 2. CALCULAR TAMAÑOS DINÁMICOS ---
+	    
+	    // El ancho de la casilla se calcula dividiendo el ancho de la ventana por las columnas.
+	    // Esto asegura que el tablero "ocupe todo el ancho".
+	    anchoCasilla = entorno.ancho()/columnas;
+	    
+	    // Hacemos que el alto de la casilla sea igual al ancho
+	    // para mantener las casillas cuadradas (como en el juego original).
+	    altoCasilla = anchoCasilla;
+	    
+	    // Calculamos el tamaño total del tablero (Servirá para más adelante)
+	    anchoTotalTablero = columnas * anchoCasilla;
+	    altoTotalTablero = filas * altoCasilla;
+	  
+
+	    // --- 3. DIBUJAR LA GRILLA ---
+	    // El tablero empieza en X=0 (borde izquierdo)
+	    int inicioX = 0;
+	    
+	    
+	    // El espacio superior que dejamos para la UI (cartas, puntaje, etc.)
+	    int margenSuperior = entorno.alto()-altoTotalTablero;
+	    // Y empieza justo debajo de nuestro margen superior
+	    int inicioY = margenSuperior ;
+
+	    // Estos for anidados sirven para dibujar la grilla, simulan una matriz de f x c  
+	    for (int f = 0; f < filas; f++) {
+	        for (int c = 0; c < columnas; c++) {
+	        	
+	        	// Calculamos el centro de la casilla (Servirá para más adelante)
+	            double x = inicioX + c * anchoCasilla + anchoCasilla / 2;
+	            double y = inicioY + f * altoCasilla + altoCasilla / 2;
+	            // Dibujamos rectangulos de un color en las filas-columnas pares y en filas-columnas impares
+	            if ((f%2 == 0 && c%2 == 0) || (f%2 != 0 && c%2 != 0  )) {
+					Color colorCesped = new Color(0, 102, 0);
+					entorno.dibujarRectangulo(x, y, anchoCasilla, altoCasilla, 0, colorCesped);
+				// Dibujamos rectangulos de otro color en las filas impares y columnas pares, y viceversa
+				}else { 
+					Color colorCesped1 = new Color(0, 50, 0);
+					entorno.dibujarRectangulo(x, y, anchoCasilla, altoCasilla, 0, colorCesped1);
+				}
+	        }
+	    }
+	}
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args)
